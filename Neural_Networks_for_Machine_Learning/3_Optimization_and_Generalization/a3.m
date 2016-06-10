02fc2d2966c35ee25317b6bb4328a9ed778b56d7
JifuZhao <jzhao59@illinois.edu>
2016-06-10 00:04:22 -0500

    rename file name for Neural Networks for Machine Learning

--- Neural_Networks_for_Machine_Learning/3_Optimization_and_Generalization/a3.m
+++ Neural_Networks_for_Machine_Learning/3_Optimization_and_Generalization/a3.m
@@ -1,3 +1,5 @@
+% This version of the starter code was published on Tuesday November 8, 01:37 UTC. It is an improved version of the one that was published on November 6, 13:37 UTC.
+
 function a3(wd_coefficient, n_hid, n_iters, learning_rate, momentum_multiplier, do_early_stopping, mini_batch_size)
   warning('error', 'Octave:broadcast');
   if exist('page_output_immediately'), page_output_immediately(1); end
@@ -6,7 +8,10 @@ function a3(wd_coefficient, n_hid, n_iters, learning_rate, momentum_multiplier, 
   from_data_file = load('data.mat');
   datas = from_data_file.data;
   n_training_cases = size(datas.training.inputs, 2);
-  if n_iters ~= 0, test_gradient(model, datas.training, wd_coefficient); end
+  if n_iters ~= 0,
+    fprintf('Now testing the gradient on the whole training set... ');
+    test_gradient(model, datas.training, wd_coefficient);
+  end
 
   % optimization
   theta = model_to_theta(model);
@@ -39,8 +44,11 @@ function a3(wd_coefficient, n_hid, n_iters, learning_rate, momentum_multiplier, 
     if mod(optimization_iteration_i, round(n_iters/10)) == 0,
       fprintf('After %d optimization iterations, training data loss is %f, and validation data loss is %f\n', optimization_iteration_i, training_data_losses(end), validation_data_losses(end));
     end
+    if optimization_iteration_i == n_iters, % check gradient again, this time with more typical parameters and with a different data size
+      fprintf('Now testing the gradient on just a mini-batch instead of the whole training set... ');
+      test_gradient(model, training_batch, wd_coefficient);
+    end 
   end
-  if n_iters ~= 0, test_gradient(model, datas.training, wd_coefficient); end % check again, this time with more typical parameters
   if do_early_stopping,
     fprintf('Early stopping: validation loss was lowest after %d iterations. We chose the model that we had then.\n', best_so_far.after_n_iters);
     theta = best_so_far.theta;
@@ -74,10 +82,31 @@ function test_gradient(model, data, wd_coefficient)
   base_theta = model_to_theta(model);
   h = 1e-2;
   correctness_threshold = 1e-5;
-  analytic_gradient = model_to_theta(d_loss_by_d_model(model, data, wd_coefficient));
-  % Test the gradient not for every element of theta, because that's a lot of work. Test for only a few elements.
+  analytic_gradient_struct = d_loss_by_d_model(model, data, wd_coefficient);
+  if size(fieldnames(analytic_gradient_struct), 1) ~= 2,
+     error('The object returned by function d_loss_by_d_model should have exactly two field names: .input_to_hid and .hid_to_class');
+  end
+  if any(size(analytic_gradient_struct.input_to_hid) ~= size(model.input_to_hid)),
+     error(sprintf(['The size of .input_to_hid of the return value of d_loss_by_d_model (currently [%d, %d]) should be same as the size of model.input_to_hid (currently [%d, %d])'], size(analytic_gradient_struct.input_to_hid), size(model.input_to_hid)));
+  end
+  if any(size(analytic_gradient_struct.hid_to_class) ~= size(model.hid_to_class)),
+     error(sprintf(['The size of .hid_to_class of the return value of d_loss_by_d_model (currently [%d, %d]) should be same as the size of model.hid_to_class (currently [%d, %d])'], size(analytic_gradient_struct.hid_to_class), size(model.hid_to_class)));
+  end
+  analytic_gradient = model_to_theta(analytic_gradient_struct);
+  if any(isnan(analytic_gradient)) || any(isinf(analytic_gradient)),
+     error('Your gradient computation produced a NaN or infinity. That is an error.')
+  end
+  % We want to test the gradient not for every element of theta, because that's a lot of work. Instead, we test for only a few elements. If there's an error, this is probably enough to find that error.
+  % We want to first test the hid_to_class gradient, because that's most likely to be correct (it's the easier one).
+  % Let's build a list of theta indices to check. We'll check 20 elements of hid_to_class, and 80 elements of input_to_hid (it's bigger than hid_to_class).
+  input_to_hid_theta_size = prod(size(model.input_to_hid));
+  hid_to_class_theta_size = prod(size(model.hid_to_class));
+  big_prime = 1299721; % 1299721 is prime and thus ensures a somewhat random-like selection of indices.
+  hid_to_class_indices_to_check = mod(big_prime * (1:20), hid_to_class_theta_size) + 1 + input_to_hid_theta_size;
+  input_to_hid_indices_to_check = mod(big_prime * (1:80), input_to_hid_theta_size) + 1;
+  indices_to_check = [hid_to_class_indices_to_check, input_to_hid_indices_to_check];
   for i = 1:100,
-    test_index = mod(i * 1299721, size(base_theta,1)) + 1; % 1299721 is prime and thus ensures a somewhat random-like selection of indices
+    test_index = indices_to_check(i);
     analytic_here = analytic_gradient(test_index);
     theta_step = base_theta * 0;
     theta_step(test_index) = h;
@@ -90,9 +119,12 @@ function test_gradient(model, data, wd_coefficient)
     fd_here = temp / h;
     diff = abs(analytic_here - fd_here);
     % fprintf('%d %e %e %e %e\n', test_index, base_theta(test_index), diff, fd_here, analytic_here);
-    if diff < correctness_threshold, continue; end
-    if diff / (abs(analytic_here) + abs(fd_here)) < correctness_threshold, continue; end
-    error(sprintf('Theta element #%d, with value %e, has finite difference gradient %e but analytic gradient %e. That looks like an error.\n', test_index, base_theta(test_index), fd_here, analytic_here));
+    if (diff > correctness_threshold) && (diff / (abs(analytic_here) + abs(fd_here)) > correctness_threshold),
+      part_names = {'input_to_hid', 'hid_to_class'};
+      error(sprintf('Theta element #%d (part of %s), with value %e, has finite difference gradient %e but analytic gradient %e. That looks like an error.\n', test_index, part_names{(i<=20)+1}, base_theta(test_index), fd_here, analytic_here));
+    end
+    if i==20, fprintf('Gradient test passed for hid_to_class. '); end
+    if i==100, fprintf('Gradient test passed for input_to_hid. '); end
   end
   fprintf('Gradient test passed. That means that the gradient that your code computed is within 0.001%% of the gradient that the finite difference approximation computed, so the gradient calculation procedure is probably correct (not certainly, but probably).\n');
 end
@@ -114,7 +146,8 @@ function ret = loss(model, data, wd_coefficient)
   % data.inputs is a matrix of size <number of inputs i.e. 256> by <number of data cases>. Each column describes a different data case. 
   % data.targets is a matrix of size <number of classes i.e. 10> by <number of data cases>. Each column describes a different data case. It contains a one-of-N encoding of the class, i.e. one element in every column is 1 and the others are 0.
 	 
-  % Before we can calculate the loss, we need to calculate a variety of intermediate values, like the state of the hidden units.
+  % Before we can calculate the loss, we need to calculate a variety of intermediate values, like the state of the hidden units. This is the
+  % forward pass, and you'll likely want to copy it into d_loss_by_d_model, because these values are also very useful for that function.
   hid_input = model.input_to_hid * data.inputs; % input to the hidden units, i.e. before the logistic. size: <number of hidden units> by <number of data cases>
   hid_output = logistic(hid_input); % output of the hidden units, i.e. after the logistic. size: <number of hidden units> by <number of data cases>
   class_input = model.hid_to_class * hid_output; % input to the components of the softmax. size: <number of classes, i.e. 10> by <number of data cases>
@@ -138,12 +171,12 @@ end
 function ret = d_loss_by_d_model(model, data, wd_coefficient)
   % model.input_to_hid is a matrix of size <number of hidden units> by <number of inputs i.e. 256>
   % model.hid_to_class is a matrix of size <number of classes i.e. 10> by <number of hidden units>
-  % data.inputs is a matrix of size <number of inputs i.e. 256> by <number of data cases>. Each column describes a different data case. 
-  % data.targets is a matrix of size <number of classes i.e. 10> by <number of data cases>. Each column describes a different data case. It contains a one-of-N encoding of the class, i.e. one element in every column is 1 and the others are 0.
+  % data.inputs is a matrix of size <number of inputs i.e. 256> by <number of data cases>
+  % data.targets is a matrix of size <number of classes i.e. 10> by <number of data cases>
 
   % The returned object is supposed to be exactly like parameter <model>, i.e. it has fields ret.input_to_hid and ret.hid_to_class. However, the contents of those matrices are gradients (d loss by d model parameter), instead of model parameters.
 	 
-  % This is the only function that you're expected to change. Right now, it just returns a lot of zeros, which is obviously not the correct output. Your job is to replace that by a correct computation.
+  % This is the only function that you're expected to change. Right now, it just returns a lot of zeros, which is obviously not the correct output. Your job is to change that.
   ret.input_to_hid = model.input_to_hid * 0;
   ret.hid_to_class = model.hid_to_class * 0;
 end
