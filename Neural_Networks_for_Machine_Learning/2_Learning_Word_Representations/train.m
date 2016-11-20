22f516a8b1d443c50b63887089f6c69afad9424c
JifuZhao <jzhao59@illinois.edu>
2016-11-19 20:08:42 -0600

    edit assignment2

--- Neural_Networks_for_Machine_Learning/2_Learning_Word_Representations/train.m
+++ Neural_Networks_for_Machine_Learning/2_Learning_Word_Representations/train.m
@@ -15,13 +15,13 @@ else
 end
 
 % SET HYPERPARAMETERS HERE.
-batchsize = 100;  % Mini-batch size.
-learning_rate = 0.1;  % Learning rate; default = 0.1.
-momentum = 0.9;  % Momentum; default = 0.9.
-numhid1 = 50;  % Dimensionality of embedding space; default = 50.
-numhid2 = 200;  % Number of units in hidden layer; default = 200.
-init_wt = 0.01;  % Standard deviation of the normal distribution
-                 % which is sampled to get the initial weights; default = 0.01
+batchsize = 100;         % Mini-batch size.
+learning_rate = 0.1;     % Learning rate; default = 0.1.
+momentum = 0.9;          % Momentum; default = 0.9.
+numhid1 = 50;            % Dimensionality of embedding space; default = 50.
+numhid2 = 200;           % Number of units in hidden layer; default = 200.
+init_wt = 0.01;          % Standard deviation of the normal distribution
+                         % which is sampled to get the initial weights; default = 0.01
 
 % VARIABLES FOR TRACKING TRAINING PROGRESS.
 show_training_CE_after = 100;
